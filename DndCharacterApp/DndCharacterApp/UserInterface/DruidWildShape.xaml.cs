ffe0b6088ca0a066859bd20b399ce8c3a8163678
WaddleGee <Georgeshapka@gmail.com>
2025-12-13 17:24:44 -0500

    continuing on druid. all buttons are added(not subclass ones yet)

---
+++ DndCharacterApp/DndCharacterApp/UserInterface/DruidWildShape.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DndCharacterApp.UserInterface
+{
+    /// <summary>
+    /// Interaction logic for DruidWildShape.xaml
+    /// </summary>
+    public partial class DruidWildShape : Window
+    {
+        public DruidWildShape()
+        {
+            InitializeComponent();
+        }
+
+        private void DruidWildShapeClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
