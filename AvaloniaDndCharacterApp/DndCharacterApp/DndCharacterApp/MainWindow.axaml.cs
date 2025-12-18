6e40724beaf5fc5ad201c2e4e97783cbe48eb8f0
WaddleGee <Georgeshapka@gmail.com>
2025-12-18 17:33:22 -0500

    switching to Avalonia for cross platform support

---
+++ AvaloniaDndCharacterApp/DndCharacterApp/DndCharacterApp/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DndCharacterApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
